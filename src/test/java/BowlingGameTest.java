d8e46d6f1fcfa8b5f160bbd6d6c4fd620188de9d
crystalfn <969274162@qq.com>
2020-09-14 20:12:33 +0800

    [fangni] feat: case of ten frame with no strike or spare

--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -50,4 +50,16 @@ public class BowlingGameTest {
         // Then
         assertEquals(154, result);
     }
+
+    @Test
+    void when_ten_frame_with_no_strike_or_spare() {
+        // Given
+        int[] numberOfHits = {10, 1, 9, 5, 3, 6, 4, 3, 6, 5, 5, 10, 10, 7, 2, 8, 1};
+
+        // When
+        int result = BowlingGame.calculateScore(numberOfHits);
+
+        // Then
+        assertEquals(149, result);
+    }
 }
